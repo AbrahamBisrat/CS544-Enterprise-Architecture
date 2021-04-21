ca28f9f73b4b9575b8d02e15d4d09c526d31e855
Payman Salek <psalek@miu.edu>
2021-04-20 20:53:07 -0500

    Added the "exercises" sub-folder

--- exercises/exercise-01-1/src/main/java/edu/miu/cs/cs544/examples/Application.java
+++ exercises/exercise-01-1/src/main/java/edu/miu/cs/cs544/examples/Application.java
@@ -1,26 +1,23 @@
-package edu.mum.cs.cs544.examples;
+package edu.miu.cs.cs544.examples;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 public class Application {
-	private static SessionFactory sessionFactory;
-
+	
+	private static final SessionFactory sessionFactory;
+	
 	static {
-		// This step will read hibernate.cfg.xml and prepare hibernate for use
-		Configuration configuration = new Configuration();
-		configuration.configure("hibernate.cfg.xml");
-		ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-		sessionFactory = configuration.buildSessionFactory(sr);
+		// If there is more than one entity, you will have to pass them as a comma delimited argument list to the method below
+		sessionFactory = HibernateUtils.getSessionFactory(Arrays.asList(Person.class));
 	}
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		// Hibernate placeholders
 		Session session = null;
