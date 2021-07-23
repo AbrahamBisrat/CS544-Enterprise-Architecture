29e51b1e3e988bbcbaf7886f7b370f47ba8d706f
Payman Salek <psalek@miu.edu>
2021-07-23 18:29:22 -0500

    Updated exercises 2 & 7

---
+++ exercises/exercise-02-1/src/main/java/edu/miu/cs/cs544/examples/HibernateUtils.java
@@ -0,0 +1,50 @@
+package edu.miu.cs.cs544.examples;
+
+import java.util.List;
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtils {
+    private static SessionFactory sessionFactory;
+    
+    private static Configuration configuration = new Configuration();
+    
+    @SuppressWarnings({ "rawtypes" })
+    public static SessionFactory getSessionFactory(List<Class> entityClasses) {
+        if (sessionFactory == null) {
+            try {
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                settings.put(Environment.URL, "jdbc:sqlserver://cs544.cs.miu.edu:1433"); //10.10.10.15
+                settings.put(Environment.USER, "612345");
+                settings.put(Environment.PASS, "612345");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.SQLServer2008Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+                
+                entityClasses.forEach(entityClass -> configuration.addAnnotatedClass(entityClass));
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return sessionFactory;
+    }
+    
+}
