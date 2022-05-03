3cbf8beb4df53625a77c79e6b20ad9a5d8191043
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-02 20:33:08 -0500

    more refactoring on with lombok

--- exercises/exercise-09-1/src/main/java/edu/miu/cs/cs544/exercise09_1/Pet.java
+++ exercises/exercise-09-1/src/main/java/edu/miu/cs/cs544/exercise09_1/Pet.java
@@ -1,38 +1,25 @@
 package edu.miu.cs.cs544.exercise09_1;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Setter @Getter
+@NoArgsConstructor
 public class Pet {
 	@Id  
     @GeneratedValue
     private int id;
     private String name;
-    
-	public Pet() {
-	}
 
 	public Pet(String name) {
 		super();
 		this.name = name;
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 }
