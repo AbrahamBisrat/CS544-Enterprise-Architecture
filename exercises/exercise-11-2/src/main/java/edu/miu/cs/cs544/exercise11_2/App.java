297c6fc368c0bf92c6bc20cb5c27c88d0918cdb3
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-03 21:55:43 -0500

    replacing direct injection with DI

--- exercises/exercise-11-2/src/main/java/edu/miu/cs/cs544/exercise11_2/App.java
+++ exercises/exercise-11-2/src/main/java/edu/miu/cs/cs544/exercise11_2/App.java
@@ -1,17 +1,24 @@
 package edu.miu.cs.cs544.exercise11_2;
 
-public class App 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
 	public static void main(String[] args) {
-		IProductService productService = new ProductService();
+//		IProductService productService = new ProductService();
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
+
+
+		IProductService productService =
 
 		Product product1 = productService.getProduct(423);
 		if (product1 != null) {
-			System.out.println(product1.toString());
+			System.out.println(product1);
 		}
 		Product product2 = productService.getProduct(239);
 		if (product2 != null) {
-			System.out.println(product2.toString());
+			System.out.println(product2);
 		}
 
 	}
