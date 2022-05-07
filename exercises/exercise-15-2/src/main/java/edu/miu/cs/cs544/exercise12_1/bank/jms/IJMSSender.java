fadfb28683cb7d10d007a43693f6bdb4acddccec
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-06 22:28:17 -0500

    Lab 15-02 boilerplate

---
+++ exercises/exercise-15-2/src/main/java/edu/miu/cs/cs544/exercise12_1/bank/jms/IJMSSender.java
@@ -0,0 +1,5 @@
+package edu.miu.cs.cs544.exercise12_1.bank.jms;
+
+public interface IJMSSender {
+	public void sendJMSMessage (String text);
+}
---
+++ exercises/exercise-15-2/src/main/java/edu/miu/cs/cs544/exercise12_1/bank/service/CurrencyConverter.java
@@ -0,0 +1,15 @@
+package edu.miu.cs.cs544.exercise12_1.bank.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CurrencyConverter implements ICurrencyConverter{
+    public double euroToDollars (double amount){
+		System.out.println("CurrencyConverter: converting "+amount+" dollars to euros");
+        return 1.57 * amount;
+    }
+    
+    public double dollarsToEuros (double amount){
+        return 0.637 * amount;
+    }
+}
