d8100209bb61613590bbcdb04e99a855d3fc6713
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-10 14:50:08 -0500

    Lab 15-x boiler plate

---
+++ exercises/exercise-15-x/src/main/java/edu/miu/cs/cs544/exercise15x/Exercise15XApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs544.exercise15x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise15XApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise15XApplication.class, args);
+    }
+
+}
