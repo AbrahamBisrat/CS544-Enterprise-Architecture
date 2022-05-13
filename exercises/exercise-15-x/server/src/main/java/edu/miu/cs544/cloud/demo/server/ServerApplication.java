10760a2e778e1efa82dd3e8e3ecba29e9b308c04
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-12 21:09:13 -0500

    Added Eureka to server and registed
    Also changed spring boot version to 2.5.3 due to compatiblity issues with Eureka

--- exercises/exercise-15-x/server/src/main/java/edu/miu/cs544/cloud/demo/server/ServerApplication.java
+++ exercises/exercise-15-x/server/src/main/java/edu/miu/cs544/cloud/demo/server/ServerApplication.java
@@ -2,7 +2,9 @@ package edu.miu.cs544.cloud.demo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServerApplication {
 
